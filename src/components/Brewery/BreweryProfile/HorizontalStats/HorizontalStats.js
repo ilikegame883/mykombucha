0d00310453418d6c3a9b3537d9b56083e68dcb05
jamesikjo <jamesikjo@gmail.com>
2022-06-16 21:35:23 -0700

    Refactor code and update styling

--- src/components/Brewery/BreweryProfile/HorizontalStats/HorizontalStats.js
+++ src/components/Brewery/BreweryProfile/HorizontalStats/HorizontalStats.js
@@ -1,106 +1,118 @@
-import {
-  Container,
-  Rating,
-  Stack,
-  Box,
-  Grid,
-  Card,
-  Typography,
-} from "@mui/material";
+import { Rating, Stack, Box, Grid, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const HorizontalStats = ({ breweryData }) => {
   const { total_products, avg, favorite_count } = breweryData;
-  const STATS = [
-    {
-      value: total_products,
-      label: "Kombuchas",
-    },
-    {
-      value: (
-        <Stack direction="row">
-          <Rating
-            value={avg}
-            readOnly
-            precision={0.25}
-            sx={{ color: "primary.main" }}
-          />
-          {avg && (
-            <Typography fontWeight="medium" color="text.secondary">
-              ({avg})
-            </Typography>
-          )}
-        </Stack>
-      ),
-      label: `Brewery Rating`,
-    },
-    {
-      value: (
-        <>
-          <FavoriteIcon fontSize="small" sx={{ mr: 1 }} color="primary" />
-          {favorite_count === 0 ? "N/A" : favorite_count}
-        </>
-      ),
-      label: "People Love This Brewery",
-    },
-  ];
-
   const theme = useTheme();
+
   return (
     <Box>
       <Grid container>
-        {STATS.map((item, i) => (
-          <Grid
-            key={i}
-            item
-            xs={12}
-            md={4}
+        <Grid
+          item
+          xs={12}
+          md={4}
+          sx={{
+            borderRight: {
+              md: `1px solid ${theme.palette.divider}`,
+            },
+            borderBottom: {
+              xs: `1px solid ${theme.palette.divider}`,
+              md: 0,
+            },
+          }}
+        >
+          <Box
             sx={{
-              borderRight: {
-                xs: 0,
-                md: `1px solid ${theme.palette.divider}`,
-              },
-              borderBottom: {
-                xs: `1px solid ${theme.palette.divider}`,
-                md: 0,
-              },
-              "&:last-child": {
-                borderRight: 0,
-                borderBottom: 0,
-              },
+              p: 1.5,
+              px: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
             }}
           >
-            <Box
-              sx={{
-                p: 2,
-                px: 4,
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "flex-end",
-              }}
-            >
-              <Box>
+            <Typography variant="h6" color="text.secondary" fontWeight="600">
+              {total_products}
+            </Typography>
+            <Typography variant="body1" color="text.primary" fontWeight="600">
+              Kombuchas
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={4}
+          sx={{
+            borderBottom: {
+              xs: `1px solid ${theme.palette.divider}`,
+              md: 0,
+            },
+            borderRight: {
+              md: `1px solid ${theme.palette.divider}`,
+            },
+          }}
+        >
+          <Box
+            sx={{
+              p: 1.5,
+              px: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+            }}
+          >
+            <Stack direction="row" alignItems="center" sx={{ height: 32 }}>
+              <Rating
+                value={avg}
+                readOnly
+                precision={0.25}
+                sx={{ color: "primary.main" }}
+              />
+              {avg && (
                 <Typography
-                  variant={"h5"}
-                  color={"text.secondary"}
-                  fontWeight="700"
-                  lineHeight={1}
-                  gutterBottom
+                  variant="body1"
+                  color="text.secondary"
+                  fontWeight="600"
+                  ml={0.5}
                 >
-                  {item.value}
+                  {" "}
+                  ({avg})
                 </Typography>
-                <Typography
-                  lineHeight={1}
-                  color="text.primary"
-                  fontWeight="500"
-                >
-                  {item.label}
-                </Typography>
-              </Box>
-            </Box>
-          </Grid>
-        ))}
+              )}
+            </Stack>
+            <Typography variant="body1" color="text.primary" fontWeight="600">
+              Brewery Rating
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Box
+            sx={{
+              p: 1.5,
+              px: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+            }}
+          >
+            <Stack direction="row" alignItems="center">
+              <FavoriteIcon fontSize="small" color="primary" />
+              <Typography
+                variant="h6"
+                color="text.secondary"
+                fontWeight="600"
+                ml={0.5}
+              >
+                {favorite_count === 0 ? "N/A" : favorite_count}
+              </Typography>
+            </Stack>
+            <Typography variant="body1" color="text.primary" fontWeight="600">
+              People Love This Brewery
+            </Typography>
+          </Box>
+        </Grid>
       </Grid>
     </Box>
   );
