7f1b9078f386b648231ccab400a7ade1f9a3a03e
jamesikjo <jamesikjo@gmail.com>
2022-08-10 18:13:33 -0700

    Add in search page limit value to search api query parameter

--- src/components/Search/SearchTable/SearchTable.js
+++ src/components/Search/SearchTable/SearchTable.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
-import { alpha } from "@mui/material/styles";
 import {
   Box,
   Divider,
   TablePagination,
-  CircularProgress,
   Table,
   TableBody,
   TableCell,
@@ -28,6 +26,7 @@ import ProductSearchNotFound from "../../ProductTable/ProductSearchNotFound";
 import { getData } from "../../../utils/fetchData";
 import getCloudinaryUrl from "../../../utils/getCloudinaryUrl";
 import CustomChips from "../../CustomChips";
+import StarIcon from "@mui/icons-material/Star";
 
 const descendingComparator = (a, b, orderBy) => {
   //a & b is an {} from row array
@@ -47,6 +46,8 @@ const getComparator = (orderDirection, orderBy) => {
     : (a, b) => -descendingComparator(a, b, orderBy);
 };
 
+const SEARCH_PAGE_LIMIT = 50;
+
 const SearchTable = ({ category }) => {
   const router = useRouter();
   const theme = useTheme();
@@ -57,43 +58,45 @@ const SearchTable = ({ category }) => {
   const [orderBy, setOrderBy] = useState("name");
   const [searchData, setSearchData] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
   useEffect(() => {
-    if (router.query?.q) {
-      setSearchQuery(router.query.q);
-      return getSearchData(router.query.q);
+    //query params as user search query received from:
+    //navbar search bar and "see more results link" from Landing Page search drop down
+    if (router.query?.search) {
+      //fill in search page bar with  user search query
+      setSearchQuery(router.query.search);
+      return getSearchData(router.query.search, SEARCH_PAGE_LIMIT);
     }
-    //if user input text inside search bar still remains when switching between kombucha or brewery tabs,
-    //fetch data with the same text from the api endpoint associated with the switched over tab
+    //if user does not clear search bar while switching between kombucha or brewery tabs,
+    //fetch search data from new tab (category) with the same text
     if (searchQuery) {
       return getSearchData(searchQuery);
     }
   }, [category]);
 
-  const getSearchData = async (str) => {
+  const getSearchData = async (str, limit) => {
     try {
-      let data = await getData(category, `search/${str}`);
+      let data = await getData(`${category}/search`, `${str}?limit=${limit}`);
       setSearchData(data);
     } catch (error) {
       console.error(error);
     }
   };
 
+  //search page search bar
   const handleSearchBar = async (e) => {
     setSearchQuery(e.target.value);
 
     if (e.target.value) {
-      //   let data = await getSearchData(e.target.value);
-      //   return setSearchData(data);
-      return await getSearchData(e.target.value);
+      return await getSearchData(e.target.value, SEARCH_PAGE_LIMIT);
     }
-    //if event.target.value === "" (empty searchbar from backspace)
-    //clear data
+    //if event.target.value === "" (i.e. empty searchbar from backspace)
+    //clear search data
     setSearchData([]);
   };
 
-  //clear search button
+  //clear search button query
   const clearSearchBar = () => {
     setSearchQuery("");
     setSearchData([]);
@@ -103,12 +106,14 @@ const SearchTable = ({ category }) => {
     setRowsPerPage(parseInt(event.target.value));
     setPage(0);
   };
+
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
+
   const noMatchesFound = searchData.length === 0;
 
-  //create link to the item page for each search result based on category name (kombucha or breweries)
+  //create links to the search result item page with category name (kombucha or breweries tab)
   const createLinkBySearchType = (resultItem) => {
     //create link for kombucha search results
     if (category === "kombucha") return `/${category}/${resultItem._id}`;
@@ -128,7 +133,7 @@ const SearchTable = ({ category }) => {
           />
         </Box>
         <Divider />
-        <Table sx={{ minWidth: 300 }} aria-label="simple table">
+        <Table aria-label="simple table">
           <TableBody>
             {searchData
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
@@ -147,7 +152,7 @@ const SearchTable = ({ category }) => {
                         <ListItemText
                           disableTypography
                           primary={
-                            <Box>
+                            <Box mb={{ xs: 0.5, sm: 0.25 }}>
                               <Link
                                 href={createLinkBySearchType(item)}
                                 passHref
@@ -157,40 +162,60 @@ const SearchTable = ({ category }) => {
                                   color="text.primary"
                                   component="a"
                                   fontWeight="700"
-                                  sx={{
-                                    textDecoration: "none",
-                                  }}
                                 >
-                                  {item.name}
+                                  {item.name}{" "}
                                 </Typography>
                               </Link>
+                              <Box sx={{ display: "inline-flex" }}>
+                                {category === "kombucha" && (
+                                  <Typography
+                                    color="text.primary"
+                                    variant={isSM ? "body1" : "body2"}
+                                    fontWeight="600"
+                                  >
+                                    {"-"}
+                                    <StarIcon
+                                      color="primary"
+                                      fontSize="small"
+                                      sx={{ verticalAlign: "sub" }}
+                                    />
+                                    {item.avg === 0 ? "N/A" : item.avg}
+                                  </Typography>
+                                )}
+                              </Box>
                             </Box>
                           }
                           secondary={
                             <Stack
                               direction="row"
-                              alignItems="center"
+                              alignItems="flex-start"
                               flexWrap="wrap"
                             >
-                              <Box pr={0.5}>
-                                <Link
-                                  href={`/breweries/${
-                                    item.brewery_slug || item.slug
-                                  }`}
-                                  passHref
-                                >
-                                  <Typography
-                                    variant="body2"
-                                    component="a"
-                                    sx={{
-                                      "&:hover": {
-                                        textDecoration: "underline",
-                                      },
-                                    }}
+                              <Box pr={1}>
+                                {category === "kombucha" ? (
+                                  <Link
+                                    href={`/breweries/${
+                                      item.brewery_slug || item.slug
+                                    }`}
+                                    passHref
                                   >
-                                    {item.brewery_name} Brewery
+                                    <Typography
+                                      variant="body2"
+                                      component="a"
+                                      sx={{
+                                        "&:hover": {
+                                          textDecoration: "underline",
+                                        },
+                                      }}
+                                    >
+                                      {item.brewery_name} Brewery
+                                    </Typography>
+                                  </Link>
+                                ) : (
+                                  <Typography variant="body2">
+                                    {item.type}
                                   </Typography>
-                                </Link>
+                                )}
                               </Box>
                               <CustomChips type={item.product_type} />
                             </Stack>
@@ -212,13 +237,19 @@ const SearchTable = ({ category }) => {
           </TableBody>
         </Table>
         <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
+          rowsPerPageOptions={[10, 20, 50]}
           component="div"
           count={searchData.length}
           rowsPerPage={rowsPerPage}
           page={page}
           onPageChange={handleChangePage}
           onRowsPerPageChange={handleChangeRowsPerPage}
+          labelRowsPerPage="# of rows"
+          sx={{
+            "& .MuiTablePagination-toolbar": {
+              pl: 0,
+            },
+          }}
         />
       </TableContainer>
     </>
