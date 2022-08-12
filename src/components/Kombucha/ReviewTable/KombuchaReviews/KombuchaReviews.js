148500773cd01e232e014ad044217d674c803043
jamesikjo <jamesikjo@gmail.com>
2022-08-11 22:40:32 -0700

    Add snack bar alert with useContext for review like button

--- src/components/Kombucha/ReviewTable/KombuchaReviews/KombuchaReviews.js
+++ src/components/Kombucha/ReviewTable/KombuchaReviews/KombuchaReviews.js
@@ -1,35 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { mutate } from "swr";
 import { useSession } from "next-auth/react";
 import { Typography, Box, CircularProgress, Divider } from "@mui/material";
 import FindInPageOutlinedIcon from "@mui/icons-material/FindInPageOutlined";
 import { patchData } from "../../../../utils/fetchData";
 import ReviewCard from "../ReviewCard";
+import { AlertContext } from "../../../../stores/context/alert.context";
+import { toggleToast } from "../../../../stores/actions";
 
-const KombuchaReviews = ({
-  kombuchaReviews,
-  isReviewDataLoading,
-  kombuchaId,
-}) => {
+const KombuchaReviews = ({ kombuchaReviews, kombuchaId, isValidating }) => {
+  const { dispatch } = useContext(AlertContext);
+
   const { data: session } = useSession();
 
   const handleClickLikeIcon = async (reviewId) => {
     if (!session) {
-      //prevent user from liking own review
-      //log in or create an account to like reviews!
-      //popup
+      dispatch(toggleToast("error", "Log in to like a review!", true));
       return;
     }
-    //when user in session clicks on review like button
-    //add review id to user like list or
-    //remove review id from list if user removes their like
-    //use useSWR mutate for live update
+    //when user in session clicks review like button,
+    //add/remove review id to from their like list
+    //use useSWR mutate to show a live update of like button
     await patchData(`reviews/${reviewId}/like`, { user: session.user._id });
     mutate(`/api/kombucha/${kombuchaId}/reviews`);
     mutate(`/api/kombucha/${kombuchaId}/reviews/top-review`);
   };
 
-  if (isReviewDataLoading) return <CircularProgress color="primary" />;
+  if (isValidating) return <CircularProgress color="primary" />;
 
   return (
     <>
