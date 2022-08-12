8e0745e28857922ee701a2d56b967c03cc68adb3
jamesikjo <jamesikjo@gmail.com>
2022-08-11 22:37:09 -0700

    Fetch session server side with unstable_getServerSession

--- pages/users/[name]/index.js
+++ pages/users/[name]/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
+import { unstable_getServerSession } from "next-auth/next";
 import { Container } from "@mui/material";
 import { getData } from "../../../src/utils/fetchData";
 import Profile from "../../../src/components/User/Profile";
 import { MainLayout } from "../../../src/components/Layout";
+import { authOptions } from "../../api/auth/[...nextauth]";
 
 const UserProfile = ({ userData }) => {
   return (
@@ -19,11 +21,17 @@ const UserProfile = ({ userData }) => {
   );
 };
 
-export async function getServerSideProps(ctx) {
-  const [userData] = await getData("users", ctx.params.name);
+export async function getServerSideProps(context) {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+  const [userData] = await getData("users", context.params.name);
   return {
     props: {
       userData,
+      session,
     },
   };
 }
