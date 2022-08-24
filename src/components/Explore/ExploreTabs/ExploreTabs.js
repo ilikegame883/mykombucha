fcc84331e1b65a0c8e7cde69bbefc2ad9a32ecc5
jamesikjo <jamesikjo@gmail.com>
2022-08-23 17:30:17 -0700

    Remove title section and add in additional props

--- src/components/Explore/ExploreTabs/ExploreTabs.js
+++ src/components/Explore/ExploreTabs/ExploreTabs.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import { Box, Typography, Tabs, Tab } from "@mui/material";
+import { Box, Tabs, Tab } from "@mui/material";
 
-const NextLinkTab = ({ label, href, tabValue }) => {
+const NextLinkTab = ({ label, href, value }) => {
   //wrap next Link component to Tab component for navigation
   //pass in value to Tab component so it can match with the current state of the page
   return (
@@ -10,54 +9,21 @@ const NextLinkTab = ({ label, href, tabValue }) => {
       <Tab
         component="a"
         label={label}
-        value={tabValue}
+        value={value}
         sx={{ fontSize: "16px", fontWeight: 700 }}
       />
     </Link>
   );
 };
 
-const ExploreTabs = ({ children, category }) => {
-  const [tabValue, setTabValue] = useState("recent");
-
-  useEffect(() => {
-    //category name received from getStaticProps
-    //setTabValue to match with the current page
-    if (category) {
-      setTabValue(category);
-    }
-  }, [category]);
-
+const ExploreTabs = ({ children, category, tabs, explore }) => {
   return (
     <>
       <Box sx={{ width: "100%" }} display="flex" flexDirection="column">
         <Box mb={2}>
-          <Box display="flex" flexWrap="wrap">
-            <Box>
-              <Typography
-                color="secondary"
-                fontWeight="600"
-                variant="subtitle1"
-              >
-                Explore
-              </Typography>
-              <Typography
-                variant="h4"
-                fontWeight="700"
-                sx={{ textTransform: "capitalize" }}
-              >
-                {tabValue === "recent"
-                  ? "Kombucha Reviews"
-                  : `${tabValue} Kombucha`}
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
-
-        <Box mb={2}>
           {/* //Tabs children must have prop named value */}
           <Tabs
-            value={tabValue}
+            value={category}
             aria-label="explore kobmucha tabs"
             variant="scrollable"
             scrollButtons="auto"
@@ -68,26 +34,14 @@ const ExploreTabs = ({ children, category }) => {
               },
             }}
           >
-            <NextLinkTab
-              label="Recent"
-              href="/kombucha/explore/recent/1"
-              value="recent"
-            />
-            <NextLinkTab
-              label="Top Rated"
-              href="/kombucha/explore/top-rated/1"
-              value="top-rated"
-            />
-            <NextLinkTab
-              label="New"
-              href="/kombucha/explore/new/1"
-              value="new"
-            />
-            <NextLinkTab
-              label="Popular"
-              href="/kombucha/explore/popular/1"
-              value="popular"
-            />
+            {tabs.map((tab) => (
+              <NextLinkTab
+                key={tab}
+                label={tab.toUpperCase()}
+                href={`/${explore.toLowerCase()}/explore/${tab}/1`}
+                value={tab}
+              />
+            ))}
           </Tabs>
         </Box>
         {children}
