c49e3e86feb0d9593114203183f06e3e16d919c4
jamesikjo <jamesikjo@gmail.com>
2022-08-23 17:28:56 -0700

    Insert title section

--- src/components/Explore/Kombucha/Kombucha.js
+++ src/components/Explore/Kombucha/Kombucha.js
@@ -1,9 +1,10 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import ExplorePagination from "../ExplorePagination";
 import ExploreTabs from "../ExploreTabs";
 import List from "../Kombucha/List";
 
 const PAGE_SIZE = 8; //# of items shown for each tab page
+const TABS = ["recent", "top-rated", "new", "popular"];
 
 const Kombucha = ({ exploreData, category, page }) => {
   const { sorted_list, total } = exploreData;
@@ -13,14 +14,31 @@ const Kombucha = ({ exploreData, category, page }) => {
 
   return (
     <Box>
-      <ExploreTabs category={category}>
+      <Box display="flex" flexWrap="wrap" mb={2}>
+        <Box>
+          <Typography color="secondary" fontWeight="600" variant="subtitle1">
+            Explore
+          </Typography>
+          <Typography
+            variant="h4"
+            fontWeight="700"
+            sx={{ textTransform: "capitalize" }}
+          >
+            {category} {category === "recent" ? "Reviews" : "Kombucha"}
+          </Typography>
+        </Box>
+      </Box>
+      <ExploreTabs category={category} tabs={TABS} explore="Kombucha">
         <List sorted_list={sorted_list} category={category} />
       </ExploreTabs>
-      <ExplorePagination
-        totalPages={totalPages}
-        currentPage={page}
-        category={category}
-      />
+      <Box mt={3} display="flex" justifyContent="center">
+        <ExplorePagination
+          totalPages={totalPages}
+          currentPage={page}
+          category={category}
+          path="kombucha"
+        />
+      </Box>
     </Box>
   );
 };
