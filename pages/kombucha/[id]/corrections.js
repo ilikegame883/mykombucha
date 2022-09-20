be3dd6554846178f94cf4360299140951720d252
jamesikjo <jamesikjo@gmail.com>
2022-09-20 12:24:12 -0700

    Remove Loading Indicator

--- pages/kombucha/[id]/corrections.js
+++ pages/kombucha/[id]/corrections.js
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 import useSWR from "swr";
 import { Box, CircularProgress, Container, Typography } from "@mui/material";
 import { MainLayout } from "../../../src/components/Layout/";
-import LoadingIndicator from "../../../src/components/LoadingIndicator";
 import CorrectionForm from "../../../src/components/Forms/CorrectionForm/CorrectionForm";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
@@ -21,72 +20,68 @@ const Corrections = () => {
 
   return (
     <MainLayout>
-      {!singleKombuchaData ? (
-        <LoadingIndicator />
-      ) : (
-        <Container maxWidth="md" sx={{ py: 5 }}>
-          <Box component="header" mb={2}>
-            <Typography variant="h6" color="secondary" gutterBottom>
-              Send Correction For
-            </Typography>
-            <Typography variant="h4" color="text.primary" fontWeight="600">
-              {singleKombuchaData[0].name}
-            </Typography>
-          </Box>
-          <Box mb={4}>
-            <Typography variant="body1" color="text.primary">
-              <b>Brewery</b>:{" "}
-              <Link
-                href={`/breweries/${singleKombuchaData[0].brewery_slug}`}
-                passHref
+      <Container maxWidth="md" sx={{ py: 5 }}>
+        <Box component="header" mb={2}>
+          <Typography variant="h6" color="secondary" gutterBottom>
+            Send Correction For
+          </Typography>
+          <Typography variant="h4" color="text.primary" fontWeight="600">
+            {singleKombuchaData[0].name}
+          </Typography>
+        </Box>
+        <Box mb={4}>
+          <Typography variant="body1" color="text.primary">
+            <b>Brewery</b>:{" "}
+            <Link
+              href={`/breweries/${singleKombuchaData[0].brewery_slug}`}
+              passHref
+            >
+              <Typography
+                variant="body1"
+                color="info.main"
+                component="a"
+                fontWeight="600"
               >
-                <Typography
-                  variant="body1"
-                  color="info.main"
-                  component="a"
-                  fontWeight="600"
-                >
-                  {singleKombuchaData[0].brewery_name}
-                </Typography>
-              </Link>
-            </Typography>
-            <Typography variant="body1" color="text.primary">
-              <b>Type</b>: {singleKombuchaData[0].product_type}
-            </Typography>
-            <Typography variant="body1" color="text.primary">
-              <b>ABV</b>: {singleKombuchaData[0].ABV}%
-            </Typography>
-            <Typography variant="body1" color="text.primary">
-              <b>Flavor</b>: {singleKombuchaData[0].flavor.join(", ")}
-            </Typography>
-          </Box>
-          <Box mb={4}>
-            <Typography
-              variant="body1"
-              color="text.primary"
-              fontWeight="700"
-              gutterBottom
-            >
-              Description:
-            </Typography>
-            <Typography
-              variant="body2"
-              color="text.primary"
-              p={1.5}
-              sx={{
-                border: "1px solid #bdbdbd",
-                borderRadius: 1,
-                bgcolor: "#F5F5F5",
-              }}
-            >
-              {singleKombuchaData[0].description}
-            </Typography>
-          </Box>
-          <Box>
-            <CorrectionForm name={singleKombuchaData[0].name} type="kombucha" />
-          </Box>
-        </Container>
-      )}
+                {singleKombuchaData[0].brewery_name}
+              </Typography>
+            </Link>
+          </Typography>
+          <Typography variant="body1" color="text.primary">
+            <b>Type</b>: {singleKombuchaData[0].product_type}
+          </Typography>
+          <Typography variant="body1" color="text.primary">
+            <b>ABV</b>: {singleKombuchaData[0].ABV}%
+          </Typography>
+          <Typography variant="body1" color="text.primary">
+            <b>Flavor</b>: {singleKombuchaData[0].flavor.join(", ")}
+          </Typography>
+        </Box>
+        <Box mb={4}>
+          <Typography
+            variant="body1"
+            color="text.primary"
+            fontWeight="700"
+            gutterBottom
+          >
+            Description:
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.primary"
+            p={1.5}
+            sx={{
+              border: "1px solid #bdbdbd",
+              borderRadius: 1,
+              bgcolor: "#F5F5F5",
+            }}
+          >
+            {singleKombuchaData[0].description}
+          </Typography>
+        </Box>
+        <Box>
+          <CorrectionForm name={singleKombuchaData[0].name} type="kombucha" />
+        </Box>
+      </Container>
     </MainLayout>
   );
 };
