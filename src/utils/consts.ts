1acc140b31829ad7854799e8fb03100dbf5a01da
jamesikjo <jamesikjo@gmail.com>
2023-04-05 11:35:18 -0700

    Move search page limit count variable to const folder

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,3 +1,3 @@
 export const EXPLORE_BREWERY_PAGE_SIZE = 8;
 export const EXPLORE_KOMBUCHA_PAGE_SIZE = 8;
-export const SEARCH_PAGE_LIMIT = 35;
+export const SEARCH_PAGE_LIMIT = 35; //pages/search/[category] - max # of items returned for each search
